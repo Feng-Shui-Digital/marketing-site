f9d843146da42d4f8b576d6115f141fd2930d568
Hank Brigham <hhbrigham5@gmail.com>
2021-11-07 08:26:37 -0700

    adding stuff for deployments

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
+  assetPrefix: isProd ? "/your-github-repo-name/" : "",
   reactStrictMode: true,
-}
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+};
