2bcc8efcf7e784d3ff7a23ed0cd64ba9c5f29c23
hankbrigham <20171832+hankbrigham@users.noreply.github.com>
2022-01-09 19:02:45 +0000

    Deploying to gh-pages from @ Feng-Shui-Digital/marketing-site@22e85b046a6dcc9968200b3c2f805954f462e62e 🚀

---
+++ _next/static/B-OwmC6u0u1OQ9NZDzB7W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/5-9b8c6f30f24ea6b4.js","static/css/5034c4035c63b400.css","static/chunks/pages/index-433bf37398ab3561.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
